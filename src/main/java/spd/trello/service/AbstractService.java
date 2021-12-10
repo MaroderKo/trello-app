a5788c6790449595c7769bfa75b86cf3086fa21b
dex <perepelytsia.artom@gmail.com>
2021-12-10 19:53:58 +0200

    Services prototype creation

---
+++ src/main/java/spd/trello/service/AbstractService.java
@@ -0,0 +1,14 @@
+package spd.trello.service;
+
+import spd.trello.domain.Resource;
+
+public abstract class AbstractService<T extends Resource> {
+
+    public abstract T create();
+
+    public abstract void print(T t);
+
+    public abstract void update (int index, T t);
+
+
+}
