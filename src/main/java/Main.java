19bc17edb8c1693d26228fee2e3c93ea954e59e4
dex <perepelytsia.artom@gmail.com>
2021-12-20 12:08:01 +0200

    Added properties support to project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,34 @@
+import org.flywaydb.core.Flyway;
 import spd.trello.domain.*;
 import spd.trello.service.CardService;
 
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        /*Card card = new Card();
-        card.setName("TestCard");
-        System.out.println(card.getName());
-        System.out.println(card);*/
-        CardService service = new CardService();
-        Card card = service.create();
-        service.print(card);
+    public static void main(String[] args) throws IOException {
+        Properties properties = loadProperties();
+        System.out.println(properties);
+        System.out.println("username="+properties.getProperty("jdbc.username"));
+        System.out.println("url="+properties.getProperty("jdbc.url"));
+
+    }
+
+    private static Flyway createFlyway(DataSource dataSource)
+    {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .load();
+    }
+
+    private static Properties loadProperties() throws IOException {
+        InputStream in = Main.class.getClassLoader()
+                .getResourceAsStream("application.properties");
+        Properties properties = new Properties();
+        properties.load(in);
+
+        return properties;
     }
 }
