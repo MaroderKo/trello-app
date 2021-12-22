1d1b188ef31a4064222ee0e2d3662f5ab48a5bf7
dex <perepelytsia.artom@gmail.com>
2021-12-22 19:48:01 +0200

    updated update and delete method from repository

--- src/main/java/spd/trello/repository/WorkspaceRepository.java
+++ src/main/java/spd/trello/repository/WorkspaceRepository.java
@@ -4,6 +4,8 @@ import spd.trello.db.ConnectionPool;
 import spd.trello.domain.Workspace;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static spd.trello.repository.RepositoryUtil.toLocalDateTime;
@@ -33,15 +35,14 @@ public class WorkspaceRepository {
     }
 
     public void update(UUID uuid, Workspace workspace) {
-        try (PreparedStatement ps = ConnectionPool.get().getConnection().prepareStatement("UPDATE workspace SET updated_by = ?, created_by = ?, created_date = ?, updated_date = ?, name = ?, description = ? WHERE id = "+uuid)) {
+        workspace.setUpdatedDate(LocalDateTime.now());
+        try (PreparedStatement ps = ConnectionPool.get().getConnection().prepareStatement("UPDATE workspace SET updated_by = ?, updated_date = ?, name = ?, description = ? WHERE id = "+uuid)) {
 
 
-            ps.setObject(2, workspace.getUpdatedBy() == null ? null : workspace.getUpdatedBy());
-            ps.setString(3, workspace.getCreatedBy());
-            ps.setObject(4, Timestamp.valueOf(workspace.getCreatedDate()), Types.TIMESTAMP);
-            ps.setObject(5, workspace.getUpdatedDate() == null ? null : Timestamp.valueOf(workspace.getUpdatedDate()), Types.TIMESTAMP);
-            ps.setString(6, workspace.getName());
-            ps.setString(7, workspace.getDescription());
+            ps.setObject(1, workspace.getUpdatedBy() == null ? null : workspace.getUpdatedBy());
+            ps.setObject(2, workspace.getUpdatedDate() == null ? null : Timestamp.valueOf(workspace.getUpdatedDate()), Types.TIMESTAMP);
+            ps.setString(3, workspace.getName());
+            ps.setString(4, workspace.getDescription());
 
             ps.execute();
 
@@ -52,7 +53,7 @@ public class WorkspaceRepository {
     }
 
     public void delete(UUID uuid) {
-        try (PreparedStatement ps = ConnectionPool.get().getConnection().prepareStatement("DELETE * FROM workspace WHERE id = " + uuid)) {
+        try (PreparedStatement ps = ConnectionPool.get().getConnection().prepareStatement("DELETE FROM workspace WHERE id = \'" + uuid+"\';")) {
             ps.execute();
         } catch (SQLException e)
         {
