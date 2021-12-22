2a3024b4e1eb067b4350a858e731baf0bb2446bc
dex <perepelytsia.artom@gmail.com>
2021-12-22 02:39:19 +0200

    make repository methods not static and fix UPDATE method

--- src/main/java/spd/trello/repository/WorkspaceRepository.java
+++ src/main/java/spd/trello/repository/WorkspaceRepository.java
@@ -11,7 +11,7 @@ import static spd.trello.repository.RepositoryUtil.toLocalDateTime;
 
 public class WorkspaceRepository {
 
-    public static void create(Workspace workspace) {
+    public void create(Workspace workspace) {
         try (PreparedStatement ps = ConnectionPool.get().getConnection().prepareStatement("INSERT INTO workspace (id, updated_by, created_by, created_date, updated_date, name, description) VALUES (?, ?, ?, ?, ?, ?, ?);")) {
 
 
@@ -24,7 +24,6 @@ public class WorkspaceRepository {
             ps.setString(7, workspace.getDescription());
 
             ps.execute();
-            //System.out.println("Workspace save status : " + ps.getResultSet().first());
 
 
         } catch (SQLException e) {
@@ -33,21 +32,19 @@ public class WorkspaceRepository {
         }
     }
 
-    public static void update(UUID uuid, Workspace workspace) {
-        try (PreparedStatement ps = ConnectionPool.get().getConnection().prepareStatement("DELETE FROM workspace WHERE id = ${uuid};INSERT INTO workspace (id, updated_by, created_by, created_date, updated_date, name, description) VALUES (?, ?, ?, ?, ?, ?, ?);")) {
+    public void update(UUID uuid, Workspace workspace) {
+        try (PreparedStatement ps = ConnectionPool.get().getConnection().prepareStatement("UPDATE workspace SET id = ?, updated_by = ?, created_by = ?, created_date = ?, updated_date = ?, name = ?, description = ? WHERE id = "+uuid)) {
 
 
             ps.setObject(1, workspace.getId(), Types.OTHER);
             ps.setObject(2, workspace.getUpdatedBy() == null ? null : workspace.getUpdatedBy());
             ps.setString(3, workspace.getCreatedBy());
             ps.setObject(4, Timestamp.valueOf(workspace.getCreatedDate()), Types.TIMESTAMP);
-            ps.setObject(5, workspace.getUpdatedDate() == null ? null : Timestamp.valueOf(workspace.getUpdatedDate()).toString());
+            ps.setObject(5, workspace.getUpdatedDate() == null ? null : Timestamp.valueOf(workspace.getUpdatedDate()), Types.TIMESTAMP);
             ps.setString(6, workspace.getName());
             ps.setString(7, workspace.getDescription());
 
             ps.execute();
-            System.out.println("Workspace save status : " + ps.getResultSet().first());
-
 
         } catch (SQLException e) {
             System.err.println("SQL EXCEPTION");
@@ -55,7 +52,7 @@ public class WorkspaceRepository {
         }
     }
 
-    public static void delete(UUID uuid) {
+    public void delete(UUID uuid) {
         try (PreparedStatement ps = ConnectionPool.get().getConnection().prepareStatement("DELETE * FROM workspace WHERE id = " + uuid)) {
             ps.execute();
         } catch (SQLException e)
@@ -66,7 +63,7 @@ public class WorkspaceRepository {
     }
 
 
-    public static Workspace read(UUID uuid) {
+    public Workspace read(UUID uuid) {
         Workspace workspace = null;
         System.out.println(uuid);
         try (PreparedStatement ps = ConnectionPool.get().getConnection().prepareStatement("SELECT * FROM workspace WHERE id = \'"+uuid.toString()+"\';")) {
@@ -86,9 +83,6 @@ public class WorkspaceRepository {
                 }
                 workspace.setName(rs.getString("name"));
                 workspace.setDescription(rs.getString("description"));
-//                workspace.setName(rs.getString("name"));
-//                workspace.setName(rs.getString("name"));
-
             }
 
 
--- src/main/java/spd/trello/service/WorkspaceService.java
+++ src/main/java/spd/trello/service/WorkspaceService.java
@@ -1,45 +1,41 @@
 package spd.trello.service;
 
-import spd.trello.domain.Card;
 import spd.trello.domain.Workspace;
 import spd.trello.repository.WorkspaceRepository;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.UUID;
 
-public class WorkspaceService extends AbstractService<Workspace>{
-        @Override
-        public Workspace create() {
-            Workspace workspace = new Workspace();
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Input name: ");
-            String name = sc.nextLine();
-            System.out.println("Input description: ");
-            String description = sc.nextLine();
-            workspace.setName(name);
-            workspace.setDescription(description);
-            System.out.println(workspace.getId());
-            WorkspaceRepository.create(workspace);
-            return workspace;
-        }
+public class WorkspaceService extends AbstractService<Workspace> {
+    WorkspaceRepository repository = new WorkspaceRepository();
+    @Override
+    public Workspace create() {
+        Workspace workspace = new Workspace();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name = sc.nextLine();
+        System.out.println("Input description: ");
+        String description = sc.nextLine();
+        workspace.setName(name);
+        workspace.setDescription(description);
+        System.out.println(workspace.getId());
+        repository.create(workspace);
+        return workspace;
+    }
 
-        @Override
-        public void update(UUID index, Workspace workspace) {
-            Workspace Workspace1 = WorkspaceRepository.read(index);
-            Workspace1.setName(workspace.getName());
-            Workspace1.setUpdatedDate(LocalDateTime.now());
+    @Override
+    public void update(UUID index, Workspace workspace) {
+        Workspace workspace1 = repository.read(index);
+        workspace1.setName(workspace.getName());
+        workspace1.setUpdatedDate(LocalDateTime.now());
 
 
-        }
+    }
 
-        public Workspace read(UUID id)
-        {
-            return WorkspaceRepository.read(id);
-        }
+    public Workspace read(UUID id) {
+        return repository.read(id);
+    }
 
-    
 
 }
